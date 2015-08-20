d9ff116f655109a04c381355df93d3366311b9e9
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2015-08-19 17:03:50 -0700

    Did many things

---
+++ AllenAlerj/AllenAlerj/resources/app.js
@@ -0,0 +1,30 @@
+﻿var home = new Vue({
+    el: '#home',
+    data: {
+        cep: '',
+        logradouro: '',
+        list: [],
+    },
+
+    methods: {
+        checkZip: function () {
+            if (this.cep.length == 8) {
+                this.$http.get('http://viacep.com.br/ws/' + this.cep + '/json/', function (data) {
+                    if (data) {
+                        this.logradouro = data.logradouro;
+                    }
+                });
+            }
+            else
+            {
+                this.logradouro = '';
+            }
+        },
+
+        addZip: function () {
+            if (this.cep && this.logradouro) {
+                this.list.push({ cep: this.cep, logradouro: this.logradouro });
+            }
+        }
+    }
+});
